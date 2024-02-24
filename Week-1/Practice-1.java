e8aed20935cc989a3a4979499a081e79f3881c0f
A7mooz <teaming04@gmail.com>
2024-02-24 15:01:44 +0300

    1st Week

---
+++ Week-1/Practice-1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+//Code the program that calculates the squares of the double numbers entered on the keyboatf and displays them as the seven different forms listed below.
+public class Practice1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Input a double: ");
+		
+		double newnum = sc.nextFloat();
+		
+		//Only with two digits after the dot
+		System.out.printf("%.2f\n", newnum*newnum);
+				
+		//Only with two digits after the dot and "+" sign in front of it
+		System.out.printf("%+.2f\n", newnum*newnum);
+				
+		//Within 10 spaces and left-sided.
+		System.out.printf("%10.2f\n", newnum*newnum);
+				
+		//within 15 spaces, three digits after the dot and left-sided
+		System.out.printf("%-15.3f\n", newnum*newnum);
+				
+		//Within 20 spaces, three digits after the dot and right-sided
+		System.out.printf("%20.3f\n", newnum*newnum);
+				
+		//with thousand separator and four digits after the dot
+		System.out.printf("%,.4f\n", newnum*newnum);
+				
+		//within 15 spaces, zeros at the beginning and three digits after the dot.
+		System.out.printf("%015.3f\n", newnum*newnum);
+
+		sc.close();
+	}
+}
---
+++ Week-1/Practice-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Code the program that calculates sum of two numbers entered on the keyboard by the user and displays the result as the five different forms listed below.
+public class Practice2 {
+	public static void main(String[] args) {
+      	Scanner sc = new Scanner(System.in);
+				
+				System.out.print("Enter two numbers: ");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+				//Within 15 spaces and zeros at the beginning
+				System.out.printf("%015d\n", a+b);
+        //Within 15 spaces and thousand separator
+        System.out.printf("%,15d\n", a+b);
+        //Within 15 spaces, thoudans separators and left-sided.
+        System.out.printf("%,15d\n", a+b);
+        //Within 12 spaces, a '+' sign in front of it and right-sided.
+        System.out.printf("%+12d\n", a+b);
+        //a '-' sign in front of it and thousand seperator
+        System.out.printf("-%,d\n", a+b);
+	}
+}
