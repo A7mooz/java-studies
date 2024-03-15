341ee38c17156f1489893c5565a874356fdc0ef2
A7mooz <teaming04@gmail.com>
2024-03-15 10:51:56 +0300

    Added practice 10 (kinda solved)

---
+++ Week-4/Practice10.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Practice10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the operation:");
+        String operation = scan.nextLine().replaceAll(" ", "");
+
+        int a = 0,b = 0;
+
+        char op = 0;
+
+        for (int i = 0; i < operation.length(); i++) {
+            if (operation.charAt(i) <= '9' && operation.charAt(i) >= '0') {
+                if (op == 0) a = a*10 + Integer.valueOf(operation.charAt(i) - '0');
+                else b = b*10 + Integer.valueOf(operation.charAt(i) - '0');
+            } else {
+               if (op == 0) op = operation.charAt(i);
+               else i = operation.length();
+            }
+        }
+
+        switch (op) {
+            case '*': System.out.println(a * b); break;
+            case '+': System.out.println(a + b); break;
+            case '-': System.out.println(a - b); break;
+            case '/': System.out.println(a / b); break;
+            default: System.out.println("Invalid operation");
+        }
+
+        scan.close();
+    }
+}
